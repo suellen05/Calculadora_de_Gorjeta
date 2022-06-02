4cfcc411dbf76b79d880f11a59a19e8fcdafe72d
Suellen <73621600+suellen05@users.noreply.github.com>
2022-06-01 21:16:16 -0300

    alterações

---
+++ appgorjeta/www/js/app.js
@@ -0,0 +1,11 @@
+function calcularGorjeta(){
+    let valorTotal= document.querySelector("#valor").value
+    valorGorjeta = valorTotal * 0.1
+    document.querySelector("#resultado").innerHTML = "Gorjeta: R$ " + valorGorjeta
+
+}
+document.querySelector("#btCalcular").addEventListener("click",function(){
+    calcularGorjeta()
+
+
+})
